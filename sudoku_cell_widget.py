e2f8ffe4ab05d8e50e05003ad9e48a4b1bbff73c
Hannes Buchholzer <buchholzer.hannes@gmail.com>
2020-01-06 22:28:44 +0000

    adden number background highlighting in red and green
    this highlighting is referred to as halo. It is only the immediate background of the number. Two object sets  self.greenNumbersHalo and self.redNumbersHalo. if a number is in one of those sets, it's halo will be drawn. green trumps red.

--- sudoku_cell_widget.py
+++ sudoku_cell_widget.py
@@ -13,16 +13,19 @@ from PyQt5.QtWidgets  import *  #QFrame, QMainWindow
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-__version__ = "0.6.1"
+__version__ = "0.6.5"
 
 
 class SudokuCellWidget(QtWidgets.QFrame):
     metrics    = {"cellWidth": 72, "cLineWidth": 2, "font": "Noto Sans", "fontSize": 20, "centralFontSize": 40}
-    textPoints = [QPoint(25,49), QPoint(11, 23), QPoint(30, 23), QPoint(49, 23), QPoint(11, 43),
-                  QPoint(30, 43), QPoint(49, 43), QPoint(11, 63), QPoint(30, 63), QPoint(49, 63)]
-    Colors   = {"normal": QColor(255,255,255), "level1": QColor(196, 219, 255),
+    textPoints = [QPoint(25,49), QPoint(11, 21), QPoint(30, 21), QPoint(49, 21), QPoint(11, 43),
+                  QPoint(30, 43), QPoint(49, 43), QPoint(11, 65), QPoint(30, 65), QPoint(49, 65)]
+    haloRects  = ([QPoint(8,4), QPoint(27,4), QPoint(46,4), QPoint(8,26), QPoint(27,26),
+                   QPoint(46,26), QPoint(8,48), QPoint(27,48), QPoint(46,48)], QSize(16,19))
+    Colors     = {"normal": QColor(255,255,255), "level1": QColor(196, 219, 255),
                   "level2": QColor(215, 158, 250), "frame": QColor(215, 158, 250),
-                "innerRect": QColor(255, 0, 0), "black": QColor(0, 0, 0)}
+                "innerRect": QColor(255, 0, 0), "black": QColor(0, 0, 0),
+                "redHalo": QColor(255, 0, 0), "greenHalo": QColor(0, 230, 0)}
 
     def __init__(self, mcell, parent, index=None):
         super().__init__(parent)
@@ -47,7 +50,15 @@ class SudokuCellWidget(QtWidgets.QFrame):
         # inner rectangel for marking square
         iWidth = tWidth-2*lineWidth
         self.innerRect = QtCore.QRect(lineWidth, lineWidth, iWidth, iWidth)
-        #print("Inner rect = ", self.innerRect)
+
+        # these numbers of the cell are printed in red
+        self.redNumbers = set()
+
+        # red number halo highlight
+        self.redNumbersHalo = set([4,6,7,8,9])
+
+        # green number halo highlight
+        self.greenNumbersHalo = set([1,2,3,5])
 
 
 
@@ -100,11 +111,35 @@ class SudokuCellWidget(QtWidgets.QFrame):
     def paintEvent(self, event: QtGui.QPaintEvent):
         super().paintEvent(event)
 
-        painter = QPainter(self)
         # draw background
-        bg = self.backgroundColor();
-        painter.setBrush(QBrush(bg));
+        painter = QPainter(self)
+        bg = self.backgroundColor()
+        painter.setBrush(QBrush(bg))
         painter.drawRect(event.rect())
+
+        # draw Halo background colors
+        redHalo    = SudokuCellWidget.Colors["redHalo"]
+        greenHalo  = SudokuCellWidget.Colors["greenHalo"]
+        haloPoints = SudokuCellWidget.haloRects[0]
+        haloSize   = SudokuCellWidget.haloRects[1]
+        if not self.myCell.isSolved():
+            # green Halo
+            for i in self.redNumbersHalo:
+                point = haloPoints[i-1]
+                rect  = QRect(point, haloSize)
+                painter.setBrush(QBrush(redHalo))
+                #painter.drawRect(rect)
+                painter.drawRoundedRect(rect, 3,3)
+
+            # red Halo
+            if not self.myCell.isSolved():
+                for i in self.greenNumbersHalo:
+                    point = haloPoints[i - 1]
+                    rect = QRect(point, haloSize)
+                    painter.setBrush(QBrush(greenHalo))
+                    # painter.drawRect(rect)
+                    painter.drawRoundedRect(rect, 3, 3)
+
 
         fName     = SudokuCellWidget.metrics["font"]
         lineWidth = SudokuCellWidget.metrics["cLineWidth"]
@@ -114,6 +149,7 @@ class SudokuCellWidget(QtWidgets.QFrame):
         fgFocusColor = SudokuCellWidget.Colors["innerRect"]
 
 
+        # draw red frame around square if it is in focus
         if self.hasFocus():
             painter.save()
             pen   = painter.pen()
@@ -126,24 +162,34 @@ class SudokuCellWidget(QtWidgets.QFrame):
             painter.restore()
 
 
+        redPen = painter.pen()
+        redPen.setColor(fgFocusColor)
+        blackPen = painter.pen()
 
+        # draw the numbers.
+        # if cell is solved draw it big and centered
         if self.myCell.isSolved():
             val   = self.myCell.value()
             point = SudokuCellWidget.textPoints[0]
             fSize = SudokuCellWidget.metrics["centralFontSize"]
-
-            if self.hasFocus():
-                pen   = painter.pen()
-                pen.setColor(fgFocusColor)
-                painter.setPen(pen)
+            if val in self.redNumbers:
+                painter.setPen(redPen)
             font.setPixelSize(fSize)
             painter.setFont(font)
             painter.drawText(point, str(val))
         else:
+            redNumbers   = self.myCell & self.redNumbers
+            blackNumbers = self.myCell - self.redNumbers
             fSize = SudokuCellWidget.metrics["fontSize"]
             font.setPixelSize(fSize)
             painter.setFont(font)
-            for i in self.myCell:
+
+            painter.setPen(blackPen)
+            for i in blackNumbers:
+                point = SudokuCellWidget.textPoints[i]
+                painter.drawText(point, str(i))
+            painter.setPen(redPen)
+            for i in redNumbers:
                 point = SudokuCellWidget.textPoints[i]
                 painter.drawText(point, str(i))
 
